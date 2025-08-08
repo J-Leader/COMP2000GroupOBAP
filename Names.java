fea086abadeefd5d62402dd7677df4b8b41a4907
wasaboo <aa606735@gmail.com>
2025-08-08 13:24:43 +1000

    added names.java
    added names.java ?

---
+++ Names.java
@@ -0,0 +1,21 @@
+public class Names {
+	public static void main(String[] Args) {
+		System.out.println("This group consists of:");
+		// Put your signature function on the below line!
+
+        PrintAdrianSig();
+		//PrintHarrisSig();
+        
+		// Put your signature function on the above line!
+
+	}
+	public static void PrintAdrianSig() {
+		System.out.println("»-(¯`·.·´¯)->Adrian<-(¯`·.·´¯)-«");
+		System.out.println("°º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸,ø¤º°`°º¤ø,¸");
+	}
+
+	public static void PrintHarrisSig() {
+		System.out.println(".--.     /\\                ____");
+		System.out.println("'--'    /__\\    (^._.^)~ <(o.o )>");
+	}
+}
